208947c52c07e4f9033dc00d05cc86a1471c91c6
Abdullah <iAbdullah.AlAmeri@gmail.com>
2019-04-15 08:00:31 -0600

    Started

---
+++ Project/DirectMethod.m
@@ -0,0 +1,56 @@
+%% info:
+
+%{
+
+This's part of APPM 3310 Matrix methods and applications project. For more
+information cehck /Info. This code will try to rank sports team using the
+first method described in the research paper (The direct method), foudn in
+pp 81. 
+
+%}
+
+
+%% housekeeping
+
+clear
+clc
+close all
+
+%% Construct matrix A:
+
+% Matrix A representes the results of interaction between teams, to do
+% that,
+
+% 1. ARIZONA CARDINALS
+% 2. ATLANTA FALCONS
+% 3. BALTIMORE RAVENS
+% 4. BUFFALO BILLS
+% 5. CAROLINA PANTHERS
+% 6. CHICAGO BEARS
+% 7. CINCINNATI BENGALS
+% 8. CLEVELAND BROWNS
+% 9. DALLAS COWBOYS
+% 10. DENVER BRONCOS
+% 11. DETROIT LIONS
+% 12. GREEN BAY PACKERS
+% 13. HOUSTON TEXANS
+% 14. INDIANAPOLIS COLTS
+% 15. JACKSONVILLE JAGUARS
+% 16. KANSAS CITY CHIEFS
+% 17. LOS ANGELES CHARGERS
+% 18. LOS ANGELES RAMS
+% 19. MIAMI DOLPHINS
+% 20. MINNESOTA VIKINGS
+% 21. NEW ENGLAND PATRIOTS
+% 22. NEW ORLEANS SAINTS
+% 23. NEW YORK GIANTS
+% 24. NEW YORK JETS
+% 25. OAKLAND RAIDERS
+% 26. PHILADELPHIA EAGLES
+% 27. PITTSBURGH STEELERS
+% 28. SAN FRANCISCO 49ERS
+% 29. SEATTLE SEAHAWKS
+% 30. TAMPA BAY BUCCANEERS
+% 31. TENNESSEE TITANS
+% 32. WASHINGTON REDSKINS
+
